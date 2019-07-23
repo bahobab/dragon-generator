2945ab0c513f713bf8e7b2697ad4e364d1dc5e8c
mono motapa <bahobab@hotmail.com>
2019-07-23 03:42:43 -0500

    added sql code to create dragonTrait table

--- bin/configure_db.sh
+++ bin/configure_db.sh
@@ -12,6 +12,7 @@ echo 'Configuring dragonstackdb'
 psql -U node_user dragonstackdb < ./bin/sql/generation.sql
 psql -U node_user dragonstackdb < ./bin/sql/dragon.sql
 psql -U node_user dragonstackdb < ./bin/sql/trait.sql
+psql -U node_user dragonstackdb < ./bin/sql/dragonTrait.sql
 
 node ./bin/insertTraits.js
 
